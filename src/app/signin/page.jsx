7d665100890c34d351d7a7dc37d5a830a02db0d2
sabujAlom <sabujalom18@gmail.com>
2026-07-05 20:03:22 +0600

    google authontication added

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -11,6 +11,7 @@ import {
   Label,
   TextField,
 } from "@heroui/react";
+import { GrGoogle } from "react-icons/gr";
 
 export default function SignInPage() {
   const onSubmit = async (e) => {
@@ -31,6 +32,12 @@ export default function SignInPage() {
     console.log({data, error})
 
   };
+
+  const handlGoogleSignIn = async() =>{
+       await authClient.signIn.social({
+          provider: 'google'
+     })
+  }
 
   return (
     <Card className="border mx-auto w-125 py-10 mt-5">
@@ -93,6 +100,9 @@ export default function SignInPage() {
           </Button>
         </div>
       </Form>
+      
+      <p className="text-center">Or</p>
+      <Button onClick={handlGoogleSignIn} variant="outline" className={'w-full'}><GrGoogle/> Sign In With Google</Button>
     </Card>
   );
 }
