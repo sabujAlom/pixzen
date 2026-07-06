95d4267647bf615ba518ce006f4f52e03a4fa1d5
sabujAlom <sabujalom18@gmail.com>
2026-07-06 16:57:59 +0600

    finally deploy

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     
-    baseURL: "https://pixzen-e5b12egw0-sabujalom18-4344s-projects.vercel.app"
+    baseURL: "https://pixzen-five.vercel.app"
 })
