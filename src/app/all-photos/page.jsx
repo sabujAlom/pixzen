ddf3834832dc2e20ee5d8a620d23fa36e515cab8
sabujAlom <sabujalom18@gmail.com>
2026-07-08 18:51:54 +0600

    category added

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,17 +1,27 @@
-import Category from "@/components/Category"
+import Category from "@/components/Category";
 import PhotoCard from "@/components/PhotoCard";
 
-const AllPhotosPage = async({searchParams}) => {
+const AllPhotosPage = async ({searchParams}) => {
     const {category} = await searchParams;
+    console.log(category)
     const res = await fetch('https://pixzen-five.vercel.app/data.json')
     const photos = await res.json()
-   
+
+    const filteredPhotos = category ? photos.filter(photo => photo.category.toLowerCase() == category.toLowerCase()) : photos
+
+
     return (
         <div>
+
             <h1 className="text-2xl font-bold m-4">All Photos</h1>
+
+            <Category/>
+
             <div className="grid grid-cols-4 gap-5">
-                {photos.map(photo=><PhotoCard key={photo.id} photo={photo}/>)}
+                {filteredPhotos.map(photo=> <PhotoCard key={photo.id} photo={photo}/>)}
             </div>
+
+            
         </div>
     );
 };
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,15 +1,24 @@
-import { Button } from '@heroui/react';
-import React from 'react';
+import { Button } from "@heroui/react";
+import Link from "next/link";
 
 const Category = async () => {
-    const res = await fetch('https://pixzen-five.vercel.app/category.json')
-    const categories = await res.json()
-    console.log(categories)
-    return (
-        <div className='mb-5 space-x-3'>
-            {categories.map(category=> <Button variant='outline' size='sm' key={category.id}>{category.name}</Button>)}
-        </div>
-    );
+  const res = await fetch("https://pixzen-five.vercel.app/category.json");
+  const categories = await res.json();
+  return (
+    <div className="mb-5 space-x-3">
+      {categories.map((category) => (
+        <Link
+          key={category.id}
+          href={`?category=${category.name.toLowerCase()}`}
+        >
+          {" "}
+          <Button variant="outline" size="sm">
+            {category.name}
+          </Button>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
