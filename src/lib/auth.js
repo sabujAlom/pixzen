52d7bbf8e930fcaf4f258e90169cb1fda934e6a0
sabujAlom <sabujalom18@gmail.com>
2026-07-08 11:32:50 +0600

    deployment

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,21 +1,21 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db("pixzen");
+
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    client,
+    client
   }),
-
-  trustedOrigins: [
-    "http://localhost:3000",
-    "https://pixzen-five.vercel.app",
-  ],
-
-  emailAndPassword: {
-    enabled: true,
-  },
-
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    },
-  },
+emailAndPassword:{
+  enabled: true
+},
+socialProviders: {
+  google: {
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  }
+}
 });
